fd0650cb1753dff3db3793f0a5bf23f160178af5
Shylock01 <luke.sanders.pd@gmail.com>
2026-05-10 23:00:55 -0400

    PWA installation fix - added 192px icon and improved manifest/SW

--- sw.js
+++ sw.js
@@ -1,18 +1,35 @@
-const CACHE_NAME = 'panopticon-v1';
+const CACHE_NAME = 'panopticon-v2';
 const ASSETS = [
   './',
-  './index.html',
-  './css/style.css',
-  './js/main.js',
-  './js/sphere.js',
-  './js/store.js',
-  './js/github.js'
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'css/style.css',
+  'js/main.js',
+  'js/sphere.js',
+  'js/store.js',
+  'js/github.js'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
