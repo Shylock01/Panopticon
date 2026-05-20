ee7fe07bd67e02d231447b0844c71e2d77e38c08
Shylock01 <luke.sanders.pd@gmail.com>
2026-05-20 12:47:53 -0400

    v2.0.4: auto-hide add button on popup visible and lower app popup bottom offset

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js v2.0.3
-const VERSION = '2.0.3';
-const CACHE_NAME = 'panopticon-v2.0.3';
+// sw.js v2.0.4
+const VERSION = '2.0.4';
+const CACHE_NAME = 'panopticon-v2.0.4';
 const ASSETS = [
   './',
   'index.html',
