0414fdcd9f7b9486cc5a5b30dd3d90ce3620e0da
Shylock01 <luke.sanders.pd@gmail.com>
2026-05-25 18:16:58 -0400

    Version 2.1.21 - Fix squished close button layout, position it on the right, and fix default reset volume mapping

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js v2.1.20
-const VERSION = '2.1.20';
-const CACHE_NAME = 'panopticon-v2.1.20';
+// sw.js v2.1.21
+const VERSION = '2.1.21';
+const CACHE_NAME = 'panopticon-v2.1.21';
 const ASSETS = [
   './',
   'index.html',
